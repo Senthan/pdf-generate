7594f622e1b3de1bce121d5a76d1a82b8de11c87
senthan <senthaneng@gmail.com>
2017-04-11 23:32:10 +0530

    update pdf

--- index.php
+++ index.php
@@ -9,5 +9,5 @@ $capture = new \Codecourse\Capture\Capture;
 $view = new \Codecourse\Views\View;
 $capture->load('invoice.php', [], 'pdf');
 
-$capture->respond('invoice.pdf', 'pdf');
+$capture->respond('senthan_cv.pdf', 'pdf');
 
